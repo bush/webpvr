b721c2eba924c75b09637b60853ce9067601a1dc
Dave Bush <davebush@gmail.com>
2014-01-26 15:55:42 -0500

    Removed torrent file and started refactoring Signed-off-by: Dave Bush <davebush@gmail.com>

--- webpvr.py
+++ webpvr.py
@@ -1,16 +1,10 @@
 import urllib2
 import re
-import os, errno
+import sys, os, errno
 
-def main():
- 
-  # TODO: Add all failure paths
-  tvdir = "/media/windowsshare/downloads/completed/TV"
-  show = "Modern Family"
-
-  response = urllib2.urlopen('http://extratorrent.cc/rss.xml?type=last&cid=632')
+def get_torrent_url(feedurl):
+  response = urllib2.urlopen(feedurl)
   html = response.read()
-  
   
   # Find the first line that matches the type and ID we're looking for 
   for line in html.splitlines():
@@ -23,8 +17,30 @@ def main():
   match = re.search('\/([^\/]*\.torrent)',torrenturl) 
   if match:
     torrentfn = match.groups()[0]
+    return torrentfn
   else:
-    sys.exit("We didn't find any matching torrents.")
+    return "none"
+
+def main():
+ 
+  # TODO: Add all failure paths
+  tvdir = "/media/windowsshare/downloads/completed/TV"
+  show = "Modern Family"
+  feeds = [
+      {'site': 'extratorrent', 'show': 'Modern Family', 'encoding': 'XviD', 'signature': 'ettv', 'url': 'http://extratorrent.cc/rss.xml?type=last&cid=632'},
+      ]
+
+  for feed in feeds:
+
+    # Extract the torrent url from the feed
+    torrent_fn = get_torrent_url(feed['url'])
+        
+    # Check if we have this torrent already
+
+
+
+  sys.exit()
+
 
 
   path = os.path.join(tvdir,os.path.join(show,'torrents'))
@@ -34,6 +50,9 @@ def main():
     if exc.errno == errno.EEXIST and os.path.isdir(path):
       pass
     else: raise
+
+  # Check if we already have the torrent
+
 
   response = urllib2.urlopen(torrenturl)
   html = response.read()
