dc6c3fb2b5e1fb857b270c35e4691ca21660e2b6
Dave Bush <davebush@gmail.com>
2014-02-07 09:43:01 -0500

    Refactored and added support for limiting the number of downloads
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- webpvr.py
+++ webpvr.py
@@ -6,7 +6,13 @@ import sys, os, errno
 import feedparser
 from subprocess import call
 
-DEBUG = 0 
+DEBUG = 1 
+TVDIR = "/media/windowsshare/downloads/completed/TV"
+FEEDS = [
+    #{'type': 'rss', 'show': 'Modern Family', 'encoding': 'XviD', 'signature': 'ettv', 'url': 'http://extratorrent.cc/rss.xml?type=last&cid=632', 'limit': 3},
+      {'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'LOL', 'url': 'https://thepiratebay.se/search/the%20big%20bang%20theory/0/7/0', 'limit': 3},
+      #{'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'eztv', 'url': 'http://eztv.it/shows/23/the-big-bang-theory/'},
+      ]
 
 # Unused, replaced with the feedparser module
 def get_torrent_url(feedurl):
@@ -28,17 +34,7 @@ def get_torrent_url(feedurl):
   else:
     return "none"
 
-# Unused
-def download_torrent(url):
-  response = urllib2.urlopen(torrent_url)
-  html = response.read()
-  full_torrent_fn = os.path.join(path,'lastest.torrent')
-  f = open(os.path.join(path,full_torrent_fn),'w')
-  f.write(html)
-  f.close
-
-
-def read_rss_feed(url,encoding,signature):
+def add_from_rss_feed(path,url,encoding,signature,limit):
   # Extract the torrent url
   feed = feedparser.parse(url)
 
@@ -46,89 +42,72 @@ def read_rss_feed(url,encoding,signature):
   #  print "There is something wrong with this feed.  Maybe the site is down ..."
   #  print feed.bozo
   #  return feed.bozo    
+  mylimit = 0
 
   for item in feed["items"]:  
 
     torrent_url = item['links'][1]['href']
+    if re.search(encoding,torrent_url) and re.search(signature,torrent_url) and mylimit < limit:
+      mylimit += 1
+      add_torrent(path,torrent_url)
 
-    if re.search(encoding,torrent_url) and re.search(signature,torrent_url):
-      if DEBUG:
-        print "Found torrent: %s" % torrent_url
-      return torrent_url 
 
-  return "none" 
-
-def read_html_feed(url,encoding,signature):
+def add_from_html_feed(path,url,encoding,signature,limit):
   
   response = urllib2.urlopen(url)
   html = response.read()
   list = re.findall(r'href="(magnet:[^"]*)"',html)
+  mylimit = 0
   for item in list:
-    if re.search(encoding,item) and re.search(signature,item):
-      return item
-  return "none"
+    if re.search(encoding,item) and re.search(signature,item) and mylimit < limit:
+      mylimit += 1
+      add_torrent(path,item)
 
-def main():
- 
-  # TODO: Add all failure paths
-  TVDIR = "/media/windowsshare/downloads/completed/TV"
-  FEEDS = [
-      {'type': 'rss', 'show': 'Modern Family', 'encoding': 'XviD', 'signature': 'ettv', 'url': 'http://extratorrent.cc/rss.xml?type=last&cid=632'},
-      {'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'LOL', 'url': 'https://thepiratebay.se/search/the%20big%20bang%20theory/0/7/0'},
-      #{'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'eztv', 'url': 'http://eztv.it/shows/23/the-big-bang-theory/'},
-      ]
+def add_torrent(path,torrent_url):
 
-  for feed_entry in FEEDS:
+  # Touch the history file
+  history_fn = os.path.join(TVDIR,'history')
+  f = open(history_fn, 'a+')
+
+  # Move on if we already downloaded the torrent file
+  if torrent_url in f.read():
+    if DEBUG:
+      print "We already got this one, moving on ..."
+    return
+
+  cmd = ['transmission-remote','-w',path,'-a',torrent_url]
+
+  try:
+    devnull = open('/dev/null', 'w')
+    if DEBUG:
+      print "Adding torrent %s" % torrent_url
+    retcode = call(cmd,stdout=devnull, stderr=devnull)
+    f.write("%s\n" % torrent_url)
+    f.close
+    devnull.close
+  except OSError, msg:
+    quit("Could not execute the above command: %s\n" % cmd)
+
+
+def get_tv_shows():
+
+  for item in FEEDS:
   
-    torrent_url = "none"
+    path = os.path.join(TVDIR,item['show'])
 
-    if feed_entry['type'] == "rss":
+    if item['type'] == "rss":
       if DEBUG:
         print "Processing rss feed ..."
-      torrent_url = read_rss_feed(feed_entry['url'],feed_entry['encoding'],feed_entry['signature'])
-    elif feed_entry['type'] == "html":
+      add_from_rss_feed(path,item['url'],item['encoding'],item['signature'],item['limit'])
+    elif item['type'] == "html":
       if DEBUG:
         print "Processing html feed ..."
-      torrent_url = read_html_feed(feed_entry['url'],feed_entry['encoding'],feed_entry['signature'])
+      add_from_html_feed(path,item['url'],item['encoding'],item['signature'],item['limit'])
 
-    if torrent_url == "none":
-      if DEBUG:
-        print "No matching torrent found"
-      continue
 
-    # Create the torrent directory if it does not exist
-    path = os.path.join(TVDIR,feed_entry['show'])
-    try:
-      os.makedirs(path)
-    except OSError as exc: # Python >2.5
-      if exc.errno == errno.EEXIST and os.path.isdir(path):
-        pass
-      else: raise
-    
-    # Touch the history file
-    history_fn = os.path.join(TVDIR,'history')
-    f = open(history_fn, 'a+')
-
-    # Move on if we already downloaded the torrent file
-    if torrent_url in f.read():
-      if DEBUG:
-        print "We already got this one, moving on ..."
-      continue
-
-    cmd = ['transmission-remote','-w',path,'-a',torrent_url]
-
-    try:
-      devnull = open('/dev/null', 'w')
-      if DEBUG:
-        print "Adding torrent %s" % torrent_url
-      retcode = call(cmd,stdout=devnull, stderr=devnull)
-      f.write("%s\n" % torrent_url)
-      f.close
-      devnull.close
-    except OSError, msg:
-      quit("Could not execute the above command: %s\n" % cmd)
-
-  
+# Main
+def main():
+  get_tv_shows()
 
 if __name__ == "__main__":
     main()
