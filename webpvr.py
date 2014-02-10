53cbe879dc531073cb8f54559720a7af212335a0
Dave Bush <davebush@gmail.com>
2014-02-09 21:35:20 -0500

    Added support for getting popular movies
    Signed-off-by: Dave Bush <davebush@gmail.com>

--- webpvr.py
+++ webpvr.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
-import urllib2
+import urllib, urllib2
+from bs4 import BeautifulSoup
 import re
 import sys, os, errno
 import feedparser
@@ -8,10 +9,11 @@ from subprocess import call
 
 DEBUG = 1 
 TVDIR = "/media/windowsshare/downloads/completed/TV"
+MOVIEDIR = "/media/windowsshare/downloads/completed/Movies"
 FEEDS = [
     #{'type': 'rss', 'show': 'Modern Family', 'encoding': 'XviD', 'signature': 'ettv', 'url': 'http://extratorrent.cc/rss.xml?type=last&cid=632', 'limit': 3},
       {'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'LOL', 'url': 'https://thepiratebay.se/search/the%20big%20bang%20theory/0/7/0', 'limit': 3},
-      #{'type': 'html', 'show': 'The Big Bang Theory', 'encoding': 'x264', 'signature': 'eztv', 'url': 'http://eztv.it/shows/23/the-big-bang-theory/'},
+      {'type': 'html', 'show': 'Modern Family', 'encoding': 'x264', 'signature': 'eztv', 'url': 'https://thepiratebay.se/search/modern%20family/0/7/0', 'limit': 3},
       ]
 
 # Unused, replaced with the feedparser module
@@ -50,6 +52,18 @@ def add_from_rss_feed(path,url,encoding,signature,limit):
     if re.search(encoding,torrent_url) and re.search(signature,torrent_url) and mylimit < limit:
       mylimit += 1
       add_torrent(path,torrent_url)
+
+
+def add_from_piratebay_most_seeded(movie):
+  url = r'http://thepiratebay.se/search/'
+  movie_url = movie.replace(' ','%20')
+  url = url+movie_url+'/0/7/0'
+  
+  response = urllib2.urlopen(url)
+  html = response.read()
+  list = re.findall(r'href="(magnet:[^"]*)"',html)
+  path = os.path.join(MOVIEDIR,movie)
+  add_torrent(path,list[0])
 
 
 def add_from_html_feed(path,url,encoding,signature,limit):
@@ -104,10 +118,63 @@ def get_tv_shows():
         print "Processing html feed ..."
       add_from_html_feed(path,item['url'],item['encoding'],item['signature'],item['limit'])
 
+def get_movie_list():
+  user_agent = 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'
+  url = 'http://www.rottentomatoes.com/dvd/top-rentals/'
+  headers = { 'User-Agent' : user_agent }
+  data = urllib.urlencode({})
+  req = urllib2.Request(url, data, headers)
+  response = urllib2.urlopen(req)
+  html = response.read()
+  #html = open('table').read() 
+  soup = BeautifulSoup(html)
+  table = soup.find("table", attrs={"class":"center movie_list rt_table"}) 
+
+  # The first tr contains the field names.
+  headings = [th.get_text() for th in table.find("tr").find_all("th")]
+
+  datasets = []
+  for row in table.find_all("tr")[1:]:
+    dataset = dict(zip(headings, (td.get_text().strip() for td in row.find_all("td"))))
+    datasets.append(dataset)
+
+  # Touch the history file
+  movies_fn = os.path.join(TVDIR,'movies')
+
+  f = open(movies_fn, 'a+')
+  movies = []
+  lines = f.read()
+
+  for row in datasets:
+  
+    movie = row['Title']
+    score = int(row['T-Meter'].strip('%'))
+
+    if score > 80:
+      #print "|%s|" % row['Title']
+      #print "|%s|" % score 
+      # Move on if we already downloaded the torrent file
+
+      if movie in lines:
+        if DEBUG:
+          print "We already downloaded %s" % movie
+        continue
+
+      movies.append(movie)
+      f.write("%s\n" % movie)
+
+  f.close
+  return movies
+
+def get_movies():
+  movies = get_movie_list()
+  for movie in movies:
+    add_from_piratebay_most_seeded(movie)
 
 # Main
 def main():
   get_tv_shows()
+  get_movies()
 
 if __name__ == "__main__":
     main()
